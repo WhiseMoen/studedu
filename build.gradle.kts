46e75e7c92170ce8b170d89c269df6988440c0ff
Sapraliev <abdulla.sapralieff@gmail.com>
2026-07-09 21:12:52 +0300

    Каркас проекта: Gradle, version catalog, wrapper

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+// Корневой build-файл: только объявление плагинов, без применения.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.ksp) apply false
+}
